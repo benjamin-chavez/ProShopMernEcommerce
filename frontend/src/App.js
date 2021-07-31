8ddadd86e1c2ff1e8b2be6be721ed789722d9003
Ben Chavez <ben.m.chavez@gmail.com>
2021-07-31 16:52:08 -0500

    session 1 end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+// import Footer from './components/Footer';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './Screens/HomeScreen';
