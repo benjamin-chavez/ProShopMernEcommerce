fdf822aaa16269fc762b2ca920c943385f248657
Ben Chavez <ben.m.chavez@gmail.com>
2021-08-04 01:00:31 -0500

    Added the Product Details/Product Show page reducer and actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,10 +3,14 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 // IMPORT REDUCERS
-import { productListReducer } from './reducers/productReducers';
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const initialState = {};
