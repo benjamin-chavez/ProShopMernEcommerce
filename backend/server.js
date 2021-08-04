d61480e715c3b4c39c79175a04b56a3487bd8893
Ben Chavez <ben.m.chavez@gmail.com>
2021-08-03 19:31:53 -0500

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,12 @@
 // const products = require('./data/products');
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -26,5 +29,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running int ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
