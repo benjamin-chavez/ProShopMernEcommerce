e4561bde59b0c322e1e56fa6694f5a679df771c5
Ben Chavez <ben.m.chavez@gmail.com>
2021-08-05 20:16:55 -0500

    created and implemented custom authentication middleware. now we can protect any route throughout the app

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,7 +30,20 @@ const authUser = asyncHandler(async (req, res) => {
 // @route:          GET /api/users/profile
 // @access:         Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('Success');
+  // res.send('Success');
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.satus(404);
+    throw new Error('User not found');
+  }
 });
 
 export { authUser, getUserProfile };
