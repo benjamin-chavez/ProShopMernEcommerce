ee5f58b1e61863bdcfdc17b9a29a7f843c8d6006
Ben Chavez <ben.m.chavez@gmail.com>
2021-08-06 00:37:25 -0500

    Added profile screen to the frontend

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,6 +10,7 @@ import {
 import { cartReducer } from './reducers/cartReducers';
 import { userLoginReducer } from './reducers/userReducers';
 import { userRegisterReducer } from './reducers/userReducers';
+import { userDetailsReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -17,6 +18,7 @@ const reducer = combineReducers({
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
