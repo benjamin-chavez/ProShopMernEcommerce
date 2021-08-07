174962c478727e2c859609782850105ba8ddd5da
Ben Chavez <ben.m.chavez@gmail.com>
2021-08-07 12:54:59 -0500

    added Create order frontend functionality

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 import { addOrderItems } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
-// router.post('/login', authUser);
 router.route('/').post(protect, addOrderItems);
 
 export default router;
--- frontend/src/Screens/PlaceOrderScreen.js
+++ frontend/src/Screens/PlaceOrderScreen.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import CheckoutSteps from '../components/CheckoutSteps';
+import { createOrder } from '../actions/orderActions';
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({ history }) => {
+  const dispatch = useDispatch();
+
   const cart = useSelector((state) => state.cart);
+
   const formatDollars = (number) => {
     return number.toLocaleString('en-US', {
       style: 'currency',
@@ -26,9 +30,28 @@ const PlaceOrderScreen = () => {
     Number(cart.shippingPrice) +
     Number(cart.taxPrice);
 
+  const orderCreate = useSelector((state) => state.orderCreate);
+  const { order, success, error } = orderCreate;
+
+  useEffect(() => {
+    if (success) {
+      history.push(`/order/${order._id}`);
+    }
+    // eslint-disable-next-line
+  }, [history, success]);
+
   const placeOrderHandler = (e) => {
-    e.preventDefault();
-    console.log('placeOrder!');
+    dispatch(
+      createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice,
+      })
+    );
   };
 
   return (
@@ -117,6 +140,9 @@ const PlaceOrderScreen = () => {
                   <Col>Total</Col>
                   <Col>{formatDollars(cart.totalPrice)}</Col>
                 </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {error && <Message variant='danger'>{error}</Message>}
               </ListGroup.Item>
               <ListGroup.Item>
                 <Button
