1ac11b853ee5e40c2ae1d4e46f1e8ecbcd5568b0
Ben Chavez <ben.m.chavez@gmail.com>
2021-08-07 23:11:32 -0500

    Created User Edit Screen and populated it with the user details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PaymentMethodScreen from './Screens/PaymentMethodScreen';
 import PlaceOrderScreen from './Screens/PlaceOrderScreen';
 import OrderScreen from './Screens/OrderScreen';
 import UserListScreen from './Screens/UserListScreen';
+import UserEditScreen from './Screens/UserEditScreen';
 
 const App = () => {
   return (
@@ -34,6 +35,7 @@ const App = () => {
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
+          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
