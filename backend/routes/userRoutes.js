12d445d02c02038b8695a43104b55ebb3a350a70
Ben Chavez <ben.m.chavez@gmail.com>
2021-08-07 19:51:43 -0500

    added backend route to request all users as an admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,11 +5,12 @@ import {
   registerUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
 } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect, isAdmin } from '../middleware/authMiddleware.js';
 
 // router.post('/login', authUser);
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, isAdmin, getUsers);
 router.route('/login').post(authUser);
 router
   .route('/profile')
