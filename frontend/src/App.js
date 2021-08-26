6fa017028de6b105d056a8575a37b33ec8bca1bd
Ben Chavez <ben.m.chavez@gmail.com>
2021-08-26 01:40:40 -0500

    create production build and direct  server.js to access the production build when live

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -46,6 +46,8 @@ const App = () => {
             component={ProductEditScreen}
           />
           <Route path='/admin/orderlist' component={OrderListScreen} />
+          <Route path='/search/:keyword' component={HomeScreen} />
+
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
